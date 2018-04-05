e8c40e1768beb1a5fdd997f82333cc21707ce397
Taichi Uragami <backpaper0@gmail.com>
2018-04-05 14:38:39 +0900

    Revert "5u13のドキュメントを追加しました"
    This reverts commit 5878206707e8a82e03f6796ac5b514f7a3624274.

--- docs/5u13/doc/_static/custom.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  var form = $('#google-search-form');
-  var q = $('#q');
-  var text = $('#text');
-  var url = 'nablarch.github.io/docs/LATEST/doc';
-  form.submit(function (event) {
-    q.val('site:' + url + ' ' + text.val());
-  });
-});
